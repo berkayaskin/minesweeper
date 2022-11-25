3db05c425baaa58cd1f1e54e7ae9fc403fefb92c
berkayaskin <berkayaskiin@gmail.com>
2022-11-25 17:30:26 +0300

    update eslint config, setup absolute path config

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+import theme from 'src/config/theme'
 import App from './App'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>,
 )
 
 // If you want to start measuring performance in your app, pass a function
