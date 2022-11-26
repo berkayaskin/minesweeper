843c61d4c479c75078f3894e65c20788f11f20d4
berkayaskin <berkayaskiin@gmail.com>
2022-11-26 13:08:37 +0300

    setup debug config for jest, update app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders Hello World text', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  const text = screen.getByText(/Hello World/i)
+    expect(text).toBeInTheDocument()
 })
