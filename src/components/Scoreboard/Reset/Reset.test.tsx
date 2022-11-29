94344f80a7d3e9ed6cca10dfd910d42342f3447e
berkayaskin <berkayaskiin@gmail.com>
2022-11-29 20:56:41 +0300

    configure testing with @testing-library and jest, create test for reset component

---
+++ src/components/Scoreboard/Reset/Reset.test.tsx
@@ -0,0 +1,33 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import React from 'react'
+import { Reset } from './Reset'
+
+describe('Reset', () => {
+  const RenderReset = () => <Reset onReset={() => null} />
+
+  it('should render elements with initial state', () => {
+    render(<RenderReset />)
+    expect(screen.getByText('🙂')).toBeInTheDocument()
+  })
+
+  it('onReset handler should be called when clicked', () => {
+    const onReset = jest.fn()
+    render(<Reset onReset={onReset} />)
+    screen.getByText('🙂').click()
+    expect(onReset).toBeCalled()
+  })
+
+  it('should change state when onMouseDown and onMouseLeave are called', () => {
+    render(<RenderReset />)
+
+    const smile = screen.getByText('🙂')
+    fireEvent.mouseDown(smile)
+    expect(screen.getByText('😮')).toBeInTheDocument()
+
+    const shocked = screen.getByText('😮')
+    fireEvent.mouseLeave(shocked)
+    expect(smile).toBeInTheDocument()
+  })
+})
+
+global.React = React
